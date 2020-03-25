571277c187bc8fcec3d7bb099edbf1b77276aa48
Alexandre <alexandre@infinity-mobile.io>
2020-03-25 11:33:23 +0100

    Fix issue with the resolve of the module fetcher

--- src/utils/fetch-module.js
+++ src/utils/fetch-module.js
@@ -1,17 +1,27 @@
 export default async function fetchModule(url) {
   const name = url.split('/').reverse()[0].match(/^(.*?)\.umd/)[1];
 
-  if (window[name]) return window[name];
+  if (window[name]) { 
+    return window[name]; 
+  }
 
   window[name] = new Promise((resolve, reject) => {
     const script = document.createElement('script');
     script.async = true;
-    script.addEventListener('load', () => {
+    script.addEventListener('load', onLoad);
+    script.addEventListener('error', onError);
+    
+    let onLoad = () => {
+      script.removeEventListener('load', onLoad);
+      script.removeEventListener('error', onError);
+
       resolve(window[name]);
-    });
-    script.addEventListener('error', () => {
-      reject(new Error(`Error loading ${url}`));
-    });
+    };
+
+    let onError = () => {
+      reject( new Error(`Error loading ${url}`) );
+    };
+
     script.src = url;
     document.head.appendChild(script);
   });
