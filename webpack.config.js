aa0e247fde6086e917a6b775f2767c7f1e912729
Alexandre Masy <alexandre@infinity-mobile.io>
2020-07-01 08:38:54 +0200

    Configure the build

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,71 @@
 const path = require('path');
 
-module.exports = {
-  devtool: 'source-map',
-  entry: './src/index.js',
-  mode: 'production',
-  resolve:{
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+const version = process.env.VERSION || require('./package.json').version
+const banner = 
+`
+@spices/ginger v${version}
+(c) ${new Date().getFullYear()} Alexandre Masy
+@license MIT
+`
+const resolve = _path => path.resolve(__dirname, _path);
+
+module.exports = [
+  {
+    entry: resolve('src/index.js'),
+    file: 'spices-ginger.js',
+    format: 'umd', 
+    env: 'development'
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'spices-ginger.js',
-    library: 'spices-ginger',
-    libraryTarget: 'umd'
+  {
+    entry: resolve('src/index.js'),
+    file: 'spices-ginger.min.js',
+    format: 'umd',
+    env: 'production'
+  },
+  {
+    entry: resolve('src/index.module.js'),
+    file: 'spices-ginger.modules.js',
+    format: 'umd',
+    env: 'development'
+  },
+  {
+    entry: resolve('src/index.module.js'),
+    file: 'spices-ginger.modules.min.js',
+    format: 'umd',
+    env: 'production'
+  },
+  {
+    entry: resolve('src/index.app.js'),
+    file: 'spices-ginger.app.js',
+    format: 'umd',
+    env: 'development'
+  },
+  {
+    entry: resolve('src/index.app.js'),
+    file: 'spices-ginger.app.min.js',
+    format: 'umd',
+    env: 'production'
+  },
+].map(genConfig);
+
+
+function genConfig(opts){
+  const config = {
+    devtool: 'source-map',
+    entry: opts.entry,
+    mode: opts.env,
+    resolve:{
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: opts.file,
+      library: 'spices-ginger',
+      libraryTarget: opts.format
+    }
   }
-};
+
+  return config;
+}
