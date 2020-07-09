d680b91bb4dcae0dc37296ca54fe31312be08b79
Alexandre Masy <alexandre@infinity-mobile.io>
2020-07-09 08:02:42 +0200

    Setup the rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -12,16 +12,68 @@ const banner =
 
 const resolve = _path => path.resolve(__dirname, _path)
 
-export default {
-  input: resolve('src/index.js'),
-  output: {
-    banner,
-    format: 'es',
-    file: 'dist/spices-ginger.min.js',
-    name: 'ginger'
+
+module.exports = [
+  {
+    entry: resolve('src/index.js'),
+    file: resolve('dist/spices-ginger.js'),
+    format: 'umd',
+    env: 'development'
   },
-  plugins: [
-    node(),
-    cjs(),
-  ]
-};
+  {
+    entry: resolve('src/index.js'),
+    file: resolve('dist/spices-ginger.min.js'),
+    format: 'umd',
+    env: 'production'
+  },
+  {
+    entry: resolve('src/index.js'),
+    file: resolve('dist/spices-ginger.esm.min.js'),
+    format: 'es',
+    env: 'production'
+  },
+
+
+  {
+    entry: resolve('src/index.module.js'),
+    file: resolve('dist/spices-ginger.modules.js'),
+    format: 'umd',
+    env: 'development'
+  },
+  {
+    entry: resolve('src/index.module.js'),
+    file: resolve('dist/spices-ginger.modules.min.js'),
+    format: 'umd',
+    env: 'production'
+  },
+  {
+    entry: resolve('src/index.host.js'),
+    file: resolve('dist/spices-ginger.host.js'),
+    format: 'umd',
+    env: 'development'
+  },
+  {
+    entry: resolve('src/index.host.js'),
+    file: resolve('dist/spices-ginger.host.min.js'),
+    format: 'umd',
+    env: 'production'
+  },
+].map(config)
+
+function config(opts){
+  const ret = {
+    input: opts.entry,
+    output: {
+      banner,
+      format: opts.format,
+      file: opts.file,
+      name: 'SpicesGinger'
+    },
+    plugins: [
+      node(),
+      cjs(),
+    ]
+  }
+
+  return ret;
+}
